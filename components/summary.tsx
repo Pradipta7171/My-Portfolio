cfad899258744e590122befbaf1aaa62d0a79d61
Pradipto Dutta <110712955+Pradipta7171@users.noreply.github.com>
2024-07-28 12:05:34 +0530

    Update summary.tsx

--- components/summary.tsx
+++ components/summary.tsx
@@ -40,7 +40,7 @@ const Summary = () => {
             />
           </motion.div>
 
-          <motion.span className="absolute bottom-0 right-0 text-4xl"
+          <motion.span className="absolute bottom-3 right-0 text-3xl"
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{
