133d73879d7980ab7f125eb9d3eebf6ded476fcf
Pradipto Dutta <110712955+Pradipta7171@users.noreply.github.com>
2024-07-28 13:07:34 +0530

    Update theme-switch.tsx

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -8,7 +8,7 @@ const ThemeSwitch = () => {
     const { theme, toggleTheme } = useTheme();
 
   return (
-    <button className="fixed bottom-5 right-5 bg-slate-300 w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] 
+    <button className="fixed bottom-5 right-5 bg-violet-200 w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] 
     border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
     onClick={toggleTheme}
     >
